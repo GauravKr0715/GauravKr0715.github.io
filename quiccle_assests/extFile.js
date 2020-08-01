2015b9f77d1863edf02f24e6aa823cec58b6355b
GauravKr0715 <laxaman.pal@gmail.com>
2020-08-01 14:30:10 +0530

    added new folder and a new file

---
+++ quiccle_assests/extFile.js
@@ -0,0 +1,22 @@
+// This file will be available on an extenal server.
+// Import it in our local files and use it for additional functionality
+
+// New
+// const background = {
+//   color: "#4cb8c4",
+//   image: "linear-gradient(to right, #4cb8c4, #3cd3ad)",
+// };
+
+// old
+const background = {
+  color: "#182b3a",
+  image: "linear-gradient(315deg, #182b3a 0%, #20a4f3 74%)",
+};
+
+const getAlertMsg = () => {
+  alert("Hello from the alert msg");
+};
+
+const getBackground = () => {
+  return background;
+};
